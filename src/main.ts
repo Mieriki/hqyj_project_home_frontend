80a2055736e4b7d29a26fa31bf756de67651218b
Mieriki <chicken1233@163.com>
2025-03-14 17:19:26 +0800

    init his

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 import 'element-plus/theme-chalk/dark/css-vars.css'
 
-axios.defaults.baseURL = 'http://localhost:8000/mugen/api'
+axios.defaults.baseURL = 'http://localhost:8003/mugen/api'
 
 const app = createApp(App)
 const pinia = createPinia()
